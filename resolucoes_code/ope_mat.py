17a9d97de5aec5f8dff952860799897a6ef78ee8
richardpedroza <149258576+richardpedroza@users.noreply.github.com>
2026-04-21 10:37:00 -0300

    atualizacao

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,16 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+num1 = int(input("Digite o primeiro numero "))
+num2 = int(input("Digite o segundo numero "))
+
+operacao = input ("Digite a operação que deseja realizar (+, -, *, /):")
+if operacao == '+':
+    print (num1 + num2)
+elif operacao == '-':
+    print (num1 - num2)
+elif operacao == '*':
+    print (num1 * num2)
+elif operacao == '/':
+    (num1 / num2)
+else:
+    print ("Operação Invalida")
--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,6 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+string = input("digite uma string: ")
+numero = int (input ("Digite um numero inteiro "))
+
+print ((string + ' ') * numero)
